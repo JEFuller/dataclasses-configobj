c4a4c63a4820767a7b913c09f70d652a3285f865
Dave Tapley <dave@jefuller.com>
2021-08-28 22:14:33 +0000

    Add to_spec

--- dataclasses_configobj/core.py
+++ dataclasses_configobj/core.py
@@ -1,5 +1,51 @@
+from inspect import signature
+from typing import List
+
+import configobj
+import typing_inspect
 from configobj import ConfigObj
 
-def to_spec():
+
+def to_spec(sections):
+    sections = [sections] if not isinstance(sections, List) else sections
+
     root = ConfigObj()
+    _to_spec(sections, root, 1, root)
     return root
+
+
+def _to_spec(sectionClasses, parent, depth, main):
+    for sectionClass in sectionClasses:
+        section = configobj.Section(parent, depth, main)
+
+        origin = typing_inspect.get_origin(sectionClass)
+        if origin == list:
+            name = '__many__'
+            isList = True
+            sectionClass = typing_inspect.get_args(sectionClass)[0]
+        elif origin == None:
+            name = sectionClass.__name__
+            isList = False
+        else:
+            raise Exception(f'Unsupported type: {origin}')
+
+        parent[name] = section
+
+        sig = signature(sectionClass)
+        items = sig.parameters.items()
+        for paramName, param in items:
+            if isList and paramName == '_name':
+                continue
+
+            paramType = param.annotation.__name__
+
+            if paramType == 'str':
+                paramType = 'string'
+            elif paramType == 'int':
+                paramType = 'integer'
+            else:
+                raise Exception(f'Unsupported type: {name}')
+
+            section.__setitem__(paramName, paramType)
+
+    return parent
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,10 +1,81 @@
+from typing import List
 import unittest
+from dataclasses import dataclass
 
-from configobj import ConfigObj
-
+import configobj
 from dataclasses_configobj import core
 
+
 class CoreTestCase(unittest.TestCase):
-    def test_to_spec(self):
-        co = core.to_spec()
-        self.assertIsInstance(co, ConfigObj)
+    def test_to_spec_1(self):
+
+        @dataclass
+        class Foo:
+            bar: str
+            pip: int
+
+        expectedSpec = list(map(str.strip, """\
+        [Foo]
+        bar = string
+        pip = integer\
+        """.split('\n')))
+
+        root = configobj.ConfigObj()
+        foo = configobj.Section(root, 1, root)
+        root['Foo'] = foo
+        foo.__setitem__('bar', 'string')
+        foo.__setitem__('pip', 'integer')
+        self.assertEqual(expectedSpec, root.write())
+
+        spec = core.to_spec(Foo)
+        self.assertEqual(expectedSpec, spec.write())
+
+    def test_to_spec_2(self):
+
+        @dataclass
+        class Foo:
+            a: str
+
+        @dataclass
+        class Bar:
+            b: int
+
+        expectedSpec = list(map(str.strip, """\
+        [Foo]
+        a = string
+        [Bar]
+        b = integer\
+        """.split('\n')))
+
+        root = configobj.ConfigObj()
+        foo = configobj.Section(root, 1, root)
+        root['Foo'] = foo
+        foo.__setitem__('a', 'string')
+        bar = configobj.Section(root, 1, root)
+        root['Bar'] = bar
+        bar.__setitem__('b', 'integer')
+        self.assertEqual(expectedSpec, root.write())
+
+        spec = core.to_spec([Foo, Bar])
+        self.assertEqual(expectedSpec, spec.write())
+
+    def test_to_spec_3(self):
+
+        @dataclass
+        class Parent:
+            other: str
+
+        @dataclass
+        class Child:
+            _name: str
+            val: str
+
+        expectedSpec = list(map(str.strip, """\
+        [Parent]
+        other = string
+        [__many__]
+        val = string\
+        """.split('\n')))
+
+        spec = core.to_spec([Parent, List[Child]])
+        self.assertEqual(expectedSpec, spec.write())
